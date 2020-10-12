2f4f6cdabba2210f97e57f9d9929aafd52c802bd
james <james@jibaoviewer.com>
2020-10-12 06:13:58 -0700

    Change package name and jenkinsfile

--- src/main/java/seleniumprac/App.java
+++ src/main/java/seleniumprac/App.java
@@ -1,4 +1,4 @@
-package seleniumtest;
+package seleniumprac;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
--- src/test/java/seleniumprac/AppTest.java
+++ src/test/java/seleniumprac/AppTest.java
@@ -1,4 +1,4 @@
-package seleniumtest;
+package seleniumprac;
 
 import org.junit.jupiter.api.Test;
 
